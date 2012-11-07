faede81b4c7f80e0b727d53a783f060739329ec4
Lily Hoskin <lilyhoskin@gmail.com>
2012-11-07 10:46:58 +0000

    Working on player input need to consider if to use events, delegates or something else for this

---
+++ Peggle/EventHanders.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace Peggle
+{
+    static class EventHanders
+    {
+        public static EventHandler<PlayerInputArgs> playerInput;
+
+        public static void raiseEvent(EventArgs e, EventType type)
+        {
+            try
+            {
+                switch (type)
+                {
+                    case EventType.PlayerInput:
+
+                        if (playerInput != null)
+                        {
+                            playerInput("EventHandler", (PlayerInputArgs)e);
+                        }
+
+                        break;
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Debug.WriteLine("Event Not Raised, Event: " + e.GetType() +" is not type " + type);
+            }
+        }
+
+        public enum EventType
+        {
+            PlayerInput
+        }
+    }
+}
---
+++ Peggle/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Peggle
+{
+    class PlayerInput : GameComponent
+    {
+        //TODO:Consider use of MultiCast delegates instead of Events 
+        Queue<PlayerInputArgs> input = new Queue<PlayerInputArgs>(1);
+
+        public PlayerInput(Game game)
+            : base(game)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            
+        }
+    }
+
+    public class PlayerInputArgs : EventArgs
+    {
+        public Vector2 arrowDirection{public get; private set;}
+
+        public PlayerInputArgs(Vector2 arrowDirection)
+        {
+            this.arrowDirection = arrowDirection;
+        }
+
+    }
+}
