3a2709469c56ba7bf2f22031694d59ca4c33342f
Lily Hoskin <lilyhoskin@gmail.com>
2012-11-17 20:45:52 +0000

    Code Cleanup

--- Peggle/CollisionResolver.cs
+++ Peggle/CollisionResolver.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using System.Diagnostics;
+using Helper;
 
 namespace Peggle
 {
--- Peggle/Helpers/VectorHelper.cs
+++ Peggle/Helpers/VectorHelper.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using System.Diagnostics;
+using Peggle;
 
-namespace Peggle
+namespace Helper
 {
     //Extension methods for Vector2
     public static class VectorHelper
