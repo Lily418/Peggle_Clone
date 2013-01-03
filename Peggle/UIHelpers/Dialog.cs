450616e368cbd4c68500e944c145c2ed0263b0d9
Lily Hoskin <lilyhoskin@gmail.com>
2013-01-03 01:42:36 +0000

    File movement

---
+++ Peggle/UIHelpers/Dialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Peggle
+{
+    static class Dialog
+    {
+        public static void gainControl(GameComponent controlTaker)
+        {
+            foreach (GameComponent gc in Game1.getComponents())
+            {
+                if (gc != controlTaker)
+                {
+                    gc.Enabled = false;
+                    if (gc is DrawableGameComponent)
+                    {
+                        ((DrawableGameComponent)gc).Visible = false;
+                    }
+                }
+
+            }
+        }
+
+        public static void returnControl(GameComponent controlGiver)
+        {
+            foreach (GameComponent gc in Game1.getComponents())
+            {
+                if (gc != controlGiver)
+                {
+                    gc.Enabled = true;
+                    if (gc is DrawableGameComponent)
+                    {
+                        ((DrawableGameComponent)gc).Visible = true;
+                    }
+                }
+
+            }
+        }
+    }
+}
